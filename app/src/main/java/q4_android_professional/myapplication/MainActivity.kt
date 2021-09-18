c9a5915a05953438584807cbb6727a67ba574929
GromZDev <Dmitryev@mail.ru>
2021-09-18 23:41:41 +0300

    Создал пустой проект. От него и будем отталкиваться.

---
+++ app/src/main/java/q4_android_professional/myapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package q4_android_professional.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
