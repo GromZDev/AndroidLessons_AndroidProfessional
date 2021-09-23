afcb9b1c9b1859410ccd24bfc9126bc899f0fbd1
GromZDev <Dmitryev@mail.ru>
2021-09-23 13:51:19 +0300

    Немного апгрейдил класс NetworkUtils.kt.

--- app/src/main/java/q4_android_professional/myapplication/utils/networkstatus/Extensions.kt
+++ app/src/main/java/q4_android_professional/myapplication/utils/networkstatus/Extensions.kt
@@ -2,3 +2,5 @@ package q4_android_professional.myapplication.utils.networkstatus
 
 /** Феерический класс, который отдаёт только пустую строку для SearchDialogFragment */
 fun String.Companion.getEmptyString(): String = ""
+
+fun String.Companion.getConnectionString(): String = "Доступность сети : "
