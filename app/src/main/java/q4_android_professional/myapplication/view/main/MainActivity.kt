4d7183ce105b95c914adc549680bbb2dfa5a9529
GromZDev <Dmitryev@mail.ru>
2021-10-13 15:23:01 +0300

    Сделал кастомный Splashscreen приложения. Причём поддерживающий автоматическую смену палитры при Dark-mode on/off. Для этого создал дневные и ночные темы, цвета, запилил зависимость в build.gradle, а также прописал в манифесте тему и android:exported="true".
    Затем сделал анимацию смахивания заставки после прохождения 2х секунд. (но код смахивания закомментил, так как после этого сверху экрана появляется странная полоса).
    
    Засетил кастомный windowSplashScreenBrandingImage.

--- app/src/main/java/q4_android_professional/myapplication/view/main/MainActivity.kt
+++ app/src/main/java/q4_android_professional/myapplication/view/main/MainActivity.kt
@@ -1,16 +1,32 @@
 package q4_android_professional.myapplication.view.main
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.support.annotation.RequiresApi
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import myapplication.historyscreen.HistoryFragment
 import q4_android_professional.myapplication.R
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(31)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        splashScreenCountdown()
+        /** После смахивания появляется белая полоска сверху, поэтому закомментил
+        slideSplashScreenToEnd(splashScreen)
+        */
 
         if (savedInstanceState == null) {
             supportFragmentManager
@@ -21,6 +37,46 @@ class MainActivity : AppCompatActivity() {
                 )
                 .commit()
         }
+    }
+
+    /** Смахивается splashscreen к концу в момент окончания его действия */
+    private fun slideSplashScreenToEnd(splashScreen: SplashScreen) {
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            val slideEnd = ObjectAnimator.ofFloat(
+                splashScreenViewProvider.view,
+                View.TRANSLATION_X,
+                0f,
+                splashScreenViewProvider.view.width.toFloat()
+            )
+            slideEnd.interpolator = AnticipateInterpolator()
+            slideEnd.duration = 500L
+            slideEnd.doOnEnd { splashScreenViewProvider.remove() }
+            slideEnd.start()
+        }
+    }
+
+    /** Открываем отсчет времени, после которого наш splashscreen уберётся */
+    private fun splashScreenCountdown() {
+        var isHideSplashScreen = false
+        object : CountDownTimer(2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                isHideSplashScreen = true
+            }
+        }.start()
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (isHideSplashScreen) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
     }
 
     /** Сетим наше меню, при этом открываю ActionBar в темах */
